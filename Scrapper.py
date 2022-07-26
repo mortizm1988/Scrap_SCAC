a2bc166b55646548968a0fe5b0b904a5915c437a
mortizm1988 <70643379+mortizm1988@users.noreply.github.com>
2022-07-26 22:11:22 +0200

    Faster by using DataFrame
    Instead of writing on a csv file in every litigation, now I just update a Dataframe and then create the csv at the end.

--- Scrapper.py
+++ Scrapper.py
@@ -5,35 +5,34 @@
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from time import sleep
-import csv
+import pandas as pd
 import list_info as list_cases
 import case_info as case
 
-email = 'YOUR-EMAIL-HERE'
-password = 'PASSWORD-HERE'
+email = 'INSERT HERE'
+password = 'INSERT HERE'
 s = Service(ChromeDriverManager().install())
 url_ = "http://securities.stanford.edu/filings.html?page="
 pn = 1
 max_pag_num=list_cases.get_total_page_number()
- 
-with open("Securities Class Action Filings 2022-01-08.csv", "w") as csvfile:
-    writer = csv.writer(csvfile)
-    writer.writerow(
-        ["Filling Name", "Filing Date", "District Court", "Exchange", "Ticker", "Link", "docker","court","judge", "case_summary", "date_classperiod_start", "date_class_period_end", "case_status_info", "date_filing", "defendant_market_status", "defendant_headquarter", "defendant_industry", "defendant_sector"])
-    while pn <= max_pag_num:
-        all_line, driver = list_cases.get_all_cases_in_one_page(url_,pn,s,email,password)
-        for oneline in all_line:
-            # extract basic case variables from table
-            name, date, court, exchange, ticker, link_case = list_cases.get_basic(oneline)
-             # go inside each case and get more detailed variables. 
-            docker,court,judge, case_summary, date_classperiod_start, date_class_period_end, case_status_info, date_filing, defendant_market_status, defendant_headquarter, defendant_industry, defendant_sector =case.extract_case_info(link_case, driver)
-            # saves case information on a csv
-            one = [name, date, court, exchange, ticker, link_case, docker,court,judge, case_summary, date_classperiod_start, date_class_period_end, case_status_info, date_filing, defendant_market_status, defendant_headquarter, defendant_industry, defendant_sector]
-            writer.writerow(one)
-            print(f"Case Saved: {name}")
-            sleep(3)
-        print(f"-- Complete: {pn} out of {max_pag_num} pages.--")
-        driver.quit()
-        pn += 1
-print("Finish")
+frame_all_cases = pd.DataFrame()
+while pn <= max_pag_num:
+    all_lines, driver = list_cases.get_all_cases_in_one_page(url_,pn,s,email,password)
+    for oneline in all_lines:
+        # extract basic case variables from table
+        name, date, court, exchange, ticker, link_case = list_cases.get_basic(oneline)
+        # go inside each case and get more detailed variables. 
+        docker,court,judge, case_summary, date_classperiod_start, date_class_period_end, case_status_info, date_filing, defendant_market_status, defendant_headquarter, defendant_industry, defendant_sector = case.extract_case_info(link_case, driver)
+        # saves case's information into the frame
+        one = pd.DataFrame([name, date, court, exchange, ticker, link_case, docker,court,judge, case_summary, date_classperiod_start, date_class_period_end, case_status_info, date_filing, defendant_market_status, defendant_headquarter, defendant_industry, defendant_sector]).T
+        frame_all_cases=pd.concat([frame_all_cases, one])
+        #print(f"Case Saved: {name}")
+        sleep(2)
+    print(f"-- Complete: {pn} out of {max_pag_num} pages.--")
+    driver.quit()
+    pn += 1
+print("DataFrame loaded")   
+frame_all_cases.columns=["Filling Name", "Filing Date", "District Court", "Exchange", "Ticker", "Link", "docker","court","judge", "case_summary", "date_classperiod_start", "date_class_period_end", "case_status_info", "date_filing", "defendant_market_status", "defendant_headquarter", "defendant_industry", "defendant_sector"]
+frame_all_cases.to_csv(data_scac,index=False, encoding='utf-8')    
+print("DataFrame saved")
 # %%
